e24549f743952bbd6e72cde14b068fd0e56e4055
hai sok <sokhai.cambodia@gmail.com>
2019-11-03 20:02:19 +0700

    config adminlte

--- resources/views/cms/blank.blade.php
+++ resources/views/cms/blank.blade.php
@@ -1,13 +1,41 @@
 @extends('layouts.cms.template')
 
 @section('content')
-<div class="pcoded-inner-content">
-    <div class="main-body">
-        <div class="page-wrapper">
-            <div class="page-body">
-                BLANK PAGE
+
+<!-- Content Header (Page header) -->
+@include('layouts.cms.content-header')
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @for($i = 0; $i < 10; $i++)
+                <!-- Default box -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Title</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                            <i class="fas fa-minus"></i></button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                            <i class="fas fa-times"></i></button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        Start creating your amazing application!
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        Footer
+                    </div>
+                    <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+                @endfor
             </div>
         </div>
     </div>
-</div>
+</section>
+<!-- /.content -->
 @endsection
--- resources/views/layouts/cms/header.blade.php
+++ resources/views/layouts/cms/header.blade.php
@@ -1,43 +1,10 @@
-<title>Admin Template</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="description" content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
-<meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
-<meta name="author" content="colorlib" />
-<!-- Favicon icon -->
-<link rel="icon" href="https://colorlib.com/polygon/admindek/plugin/assets/images/favicon.ico" type="image/x-icon">
-<!-- Google font-->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
-<!-- Required Fremwork -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/bower_components/bootstrap/css/bootstrap.min.css') }}">
-<!-- waves.css -->
-<link rel="stylesheet" href="{{ asset('plugin/cms/assets/pages/waves/css/waves.min.css') }}" type="text/css" media="all">
-<!-- feather icon -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/icon/feather/css/feather.css') }}">
-<!-- ico font -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/icon/icofont/css/icofont.css') }}">
-<!-- font-awesome-n -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/font-awesome-n.min.css') }}">
-<!-- Chartlist chart css -->
-<link rel="stylesheet" href="{{ asset('plugin/cms/bower_components/chartist/css/chartist.css') }}" type="text/css" media="all">
-<!-- Style.css -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/style.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/widget.css') }}">
-<!-- Dropify.css -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/dropify.css') }}">
-<!-- jquery-ui.css -->
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/jquery-ui.css') }}">
-
-{{-- script onlline --}}
-{{-- <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"> --}}
-<link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/css/bootstrap-datepicker.css') }}">
-
-<style>
-.datepicker {
-    z-index:99999999 !important;
-}
-</style>
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('cms/plugins/fontawesome-free/css/all.min.css')}}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ asset('cms/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('cms/dist/css/adminlte.min.css')}}">
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
