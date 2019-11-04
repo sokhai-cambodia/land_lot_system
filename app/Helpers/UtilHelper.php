9226910360321f6b6419c91e1af5b25263205be7
hai sok <sokhai.cambodia@gmail.com>
2019-11-04 14:36:39 +0700

    add comment to utilhelper

--- app/Helpers/UtilHelper.php
+++ app/Helpers/UtilHelper.php
@@ -5,27 +5,47 @@ use Illuminate\Support\Facades\Route;
 Class UtilHelper
 {
 
+    /**
+     * use to selected on selected html tag
+     */
     public static function selected($value, $compareValue) {
         return  $value == $compareValue ? 'selected' : '';
     }
 
+    /**
+     * use to checked on checkbox html tag
+     */
     public static function checked($value, $compareValue) {
         return  $value == $compareValue ? 'checked' : '';
     }
 
+    /**
+     * check if has value. use it for on create or edit form
+     */
     public static function hasValue($value, $defaultValue = '') {
         return  ($value != '' && $value != null) ? $value : $defaultValue;
     }
 
+    /**
+     * use to generate slug for utf8
+     */
     public static function slug($string) {
         return preg_replace('/\s+/u', '-', trim($string));
     }
 
+    /**
+     * use to active sidebar
+     * @hasTreeView = true: if sidebar has sub sidebar
+     */
     public static function activeSideBar($routeArr, $hasTreeView = false) {
         $str = $hasTreeView ? 'menu-open' : 'active';
         return in_array(request()->route()->getName(), $routeArr) ? $str : '';
     }
 
+    /**
+     * prevent error when missing route name
+     * missing route name will generate '#'
+     */
     public static function route($name, $params = []) {
         return Route::has($name) ? route($name, $params) : '#';
     }
