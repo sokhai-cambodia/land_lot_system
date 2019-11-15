dcfb7146190c9628f525b0102b2d30745f41fc0d
hai sok <sokhai.cambodia@gmail.com>
2019-11-15 16:23:53 +0700

    display auth image on sidebar

--- resources/views/layouts/cms/sidebar.blade.php
+++ resources/views/layouts/cms/sidebar.blade.php
@@ -12,7 +12,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('cms/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ Auth::user()->getPhoto() }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="{{ route('profile.change-password') }}" class="d-block">{{ Auth::user()->last_name }} {{ Auth::user()->first_name }}</a>
